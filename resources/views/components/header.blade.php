0bea94f0ee226448334ae5c1038db8f2f3e4217e
atalamahardika <atalamahardika24@gmail.com>
2025-05-15 20:03:55 +0700

    format header di landing page

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,40 @@
+<div class="header" style="background-color: #F2F5FC">
+    <div class="container">
+        <div class="navbar">
+            <a class="navbar-brand" href="{{ url('/')}}">
+                <img src="{{ asset('adn-hd.png') }}" alt="Logo" class="logo" style="width: 48px; height: 48px;">
+                <span class="title fw-semibold" style="color: #15400e; font-family: Nunito;">Aliansi Dosen Nahada</span>
+            </a>
+
+            <div class="navbar-menu">
+                <ul class="nav gap-3">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/')}}">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/')}}">Tentang Kami</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/')}}">Divisi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/')}}">Publikasi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/')}}">Berita</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/')}}">Tri Dharma PT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/')}}">Kontak</a>
+                    </li>
+                    {{-- <button class="btn btn-primary">Masuk</button> --}}
+                    <li class="nav-item">
+                        <button class="btn btn-primary">Masuk</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>

ed25b33ffe8f14d9285fe0b9f8f2e9f8dcd3c24c
atalamahardika <atalamahardika24@gmail.com>
2025-05-18 00:00:18 +0700

    Menambahkan routing beberapa halaman dalam landing page

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BeritaController;
+use App\Models\Berita;
 
 Route::get('/', function () {
-    return view('welcome');
+    $beritas = Berita::latest()->take(8)->get();
+    return view('pages.landing', compact('beritas'));
 });
-Route::get('/test', function () {
-    return view('test');
+Route::get('/tridharma', function ()  {
+    return view('pages.tridharma');
 });
+Route::get('/publikasi', function() {
+    return view('pages.publication');
+});
+Route::get('/divisi', function() {
+    return view('pages.division');
+});
+
+
+Route::get('/berita', [BeritaController::class, 'index'])->name('pages.news');
+Route::get('/berita/{slug}', [BeritaController::class, 'show'])->name('pages.news-detail');

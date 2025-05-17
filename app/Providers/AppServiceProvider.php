2994271bf9cbbeafc572060c9f8dfb3054938c49
atalamahardika <atalamahardika24@gmail.com>
2025-05-17 23:58:34 +0700

    Menambahkan format tanggal dalam bahasa indonesia

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Pastikan locale Indonesia aktif
+        Carbon::setLocale('id');
+        setlocale(LC_TIME, 'id_ID.utf8'); // Untuk beberapa server mungkin perlu ini juga
     }
 }

60e0fc062f9df029e411c105e23149b92012c28d
atalamahardika <atalamahardika24@gmail.com>
2025-05-19 18:35:58 +0700

    Mengubah isi konten dengan lorem ipsum

--- database/factories/BeritaFactory.php
+++ database/factories/BeritaFactory.php
@@ -16,16 +16,30 @@ class BeritaFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    
-     protected $model = Berita::class;
-    
-     public function definition()
+
+    protected $model = Berita::class;
+
+    public function definition()
     {
         $judul = $this->faker->sentence(8);
+        $lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.
+
+                    Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.
+
+                    Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales.
+
+                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+
+                    Ut velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut orci vel massa suscipit pulvinar. Nulla sollicitudin. Fusce varius, ligula non tempus aliquam, nunc turpis ullamcorper nibh, in tempus sapien eros vitae ligula.
+
+                    Pellentesque rhoncus nunc et augue. Integer id felis. Curabitur aliquet pellentesque diam. Integer quis metus vitae elit lobortis egestas. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+
+                    Morbi vel erat non mauris convallis vehicula. Nulla et sapien. Integer tortor tellus, aliquam faucibus, convallis id, congue eu, quam. Mauris ullamcorper felis vitae erat. Proin feugiat, augue non elementum posuere, metus purus iaculis lectus, et tristique ligula justo vitae magna.
+                    ';
         return [
             'judul' => $judul,
             'slug' => Str::slug($judul),
-            'isi' => $this->faker->paragraph,
+            'isi' => $lorem,
             'gambar' => '1582345286151-IMG-20200222-WA0016_detail.png', // atau bisa kosong/null kalau nullable
             'sumber' => $this->faker->sentence(3),
         ];
